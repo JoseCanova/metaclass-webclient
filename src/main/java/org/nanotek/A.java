01b559b033729898c0883a94790842f8c296d5bb
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-01-27 14:28:25 -0300

    mounting project structure

---
+++ src/main/java/org/nanotek/A.java
@@ -0,0 +1,9 @@
+package org.nanotek;
+
+public class A {
+
+	public A() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/test/java/test/org/nanotek/TestA.java
@@ -0,0 +1,9 @@
+package test.org.nanotek;
+
+public class TestA {
+
+	public TestA() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
