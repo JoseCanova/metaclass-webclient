32def39f66cdea3a08b34a25d797aa5c04dbfe7b
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-01-27 15:06:48 -0300

    updated project configuration

--- src/main/java/org/nanotek/WebclientApp.java
+++ src/main/java/org/nanotek/WebclientApp.java
@@ -1,5 +1,6 @@
 package org.nanotek;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -7,5 +8,8 @@ public class WebclientApp {
 
 	public WebclientApp() {
 	}
-
+	public static void main(String[] args) {
+		SpringApplication.run(WebclientApp.class, args);
+	}
+	
 }
