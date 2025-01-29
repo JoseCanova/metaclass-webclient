66375883ad67e1f32d1721cf6c0b86f3820aa442
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-01-29 14:55:21 -0300

    update tasks

--- src/main/java/org/nanotek/metaclass/webclient/MetaClassWebClientService.java
+++ src/main/java/org/nanotek/metaclass/webclient/MetaClassWebClientService.java
@@ -1,14 +1,24 @@
 package org.nanotek.metaclass.webclient;
 
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.nanotek.meta.model.rdbms.RdbmsMetaClass;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import jakarta.validation.Validator;
+import reactor.netty.http.client.HttpClientRequest;
+
+
+//TODO: insert validation and correct pattern for mono-clients.
+//TODO: clean up "json model" from entity-related anotattions
 @Service
 public class MetaClassWebClientService {
 
@@ -21,11 +31,41 @@ public class MetaClassWebClientService {
 	@Autowired
 	private BaseMetaClassUriService baseMetaClassUriService;
 	
+	@Autowired
+	Validator validator;
+	
+	
+	private Set<?> validateMetaClass(RdbmsMetaClass mc) {
+		return validator.validate(mc);
+	}
+	
+	
 	public MetaClassWebClientService() {
 	}
 	
 	public List<RdbmsMetaClass> retrieveMetaClasses(){
-		return null;
+		var theList = new ArrayList<RdbmsMetaClass>();
+		webClient.get()
+				.uri(baseMetaClassUriService.buildBaseUri()
+						.toString()
+						.concat("/")
+						.concat (baseMetaClassUriService.getBaseMetaClassPath()))
+				.accept(MediaType.APPLICATION_JSON)
+				.httpRequest(httpRequest -> {
+					HttpClientRequest reactorRequest = httpRequest.getNativeRequest();
+					reactorRequest.responseTimeout(Duration.ofSeconds(20));
+				})
+				.retrieve()
+				.bodyToMono(List.class)
+				.subscribe(l -> convertAndPopulateList(l , theList));
+		return theList;
+	}
+
+
+	private void convertAndPopulateList(List<?> l, List<RdbmsMetaClass> arrayList) {
+		l.stream()
+		.map(jsonNode -> objectMapper.convertValue(jsonNode, RdbmsMetaClass.class))
+		.forEach(mc -> arrayList.add(mc));
 	}
 	
 	
